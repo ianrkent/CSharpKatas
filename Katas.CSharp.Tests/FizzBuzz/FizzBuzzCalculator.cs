e58ddc1b073f6aee01f7247fe66536438a70014a
Ian Kent <ianrkent@gmail.com>
2016-10-28 09:30:53 +0100

    Added FizzBuzz kata

---
+++ Katas.CSharp.Tests/FizzBuzz/FizzBuzzCalculator.cs
@@ -0,0 +1,14 @@
+﻿namespace Katas.CSharp.Tests.FizzBuzz
+{
+    public class FizzBuzzCalculator 
+    {
+        public string FizzBuzzMe(int i)
+        {
+            if (i % 3 == 0 && i % 5 == 0) return "FizzBuzz";
+            if (i % 3 == 0) return "Fizz";
+            if (i % 5 == 0) return "Buzz";
+
+            return i.ToString();
+        }
+    }
+}
---
+++ Katas.CSharp.Tests/FizzBuzz/FizzBuzzShould.cs
@@ -0,0 +1,29 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace Katas.CSharp.Tests.FizzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzShould
+    {
+        [TestCase(1, "1")]
+        [TestCase(2, "2")]
+        [TestCase(3, "Fizz")]
+        [TestCase(6, "Fizz")]
+        [TestCase(9, "Fizz")]
+        [TestCase(5, "Buzz")]
+        [TestCase(15, "FizzBuzz")]
+        [TestCase(30, "FizzBuzz")]
+        public void ReturnCorrectValue(int value, string expectedResult)
+        {
+            // arrange
+            var fizzBuzzCalculator = new FizzBuzzCalculator();
+            
+            // act
+            string result = fizzBuzzCalculator.FizzBuzzMe(value);
+
+            // assert
+            result.Should().Be(expectedResult);
+        }
+    }
+}
